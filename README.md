1f1305681c45e2aff351e505c32e5b5abc38d39e
sg-hk <rubenruben000@proton.me>
2024-09-25 17:23:48 +0800

    latest

--- README.md
+++ README.md
@@ -3,20 +3,22 @@
 #### Description
 /!\ very early stage work /!\\
 
-As memory-light a vocabulary review app as I can come up with. ccq runs in the terminal, reads and writes to a simple text file, and uses an external algorithm to change just one field, the review date. The ccq-mine fennel script is meant to be compiled to lua and used with mpv to populate ccq's database. Details below
+As memory-light a vocabulary review app as I can come up with. ccq runs in the terminal, reads and writes to a simple text file, and uses an external algorithm to change just one field, the review date. More functionalities will be added in time.
 
 #### Functionalities
 ccq:
 - reads a csv file where every word is one line
 - loads to memory the lines where the last integer (YYYYMMDD) <= today
-- parses the line's fields, and quizzes the user randomly *\[randomness to be implemented\]*
-- \<a\>gain, \<h\>ard, \<g\>ood, and \<e\>asy let ccq know your performance
-- calls the fsrs algorithm when reviews end *\[scheduler to be implemented\]*
+- parses the line's fields, and quizzes the user
+- \<a\>gain, \<h\>ard, \<g\>ood, and \<e\>asy let ccq know your performance *\[to change to just pass/fail\]*
+- calls the fsrs algorithm when reviews end *\[for now just implementing I*2 algo\]*
 - overwrites the last integer *\[updating to be implemented\]*
 
+ccq-parse: *\[all functionalities yet to be implemented\]*
+- takes string(s) of Chinese (Mandarin) text as argument
+- lets user look up parsed entries in yomitan dictionaries
+- words can then be added to the ccq database for review
+
 ccq-mine: *\[all functionalities yet to be implemented\]*
-- adds vim-like bindings to mpv
-- directly searches subtitles in yomitan dictionaries
-- enables user to choose strings within the definitions
-- appends the word and its information to ccq's csv file
-additionally, for non-video based media (song lyrics; audiobooks), it turns the subtitles into "books" by displaying previous/next subtitles
+- transforms mpv subtitles so that ccq-parse can search them
+additionally, for non-video based media (song lyrics; audiobooks), it turns the subtitles into "books" by displaying previous/next subtitles as a page
--- ccq.c
+++ ccq.c
@@ -2,7 +2,8 @@
 * Author: sg-hk
 * Minimalistic flashcards in the terminal */
 
-#include "fsrs_scheduler.c"
+#include <dirent.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,15 +11,126 @@
 #include <time.h>
 #include <unistd.h>
 
-#define RELATIVE_PATH "/.local/share/ccq/deck"
-#define MAX_LINE_LENGTH 2048
 #define INITIAL_CAPACITY 100 
+#define MAX_LINE_LENGTH 2048
+#define CCQ_PATH "/.local/share/ccq/"
+
+typedef struct ReviewData {
+	char** due;
+	int wcount;
+} ReviewData;
+
+typedef struct ParsedLine {
+	char* word;
+	char* reading;
+	char* definition;
+	char* sentence;
+	char* sreading;
+	char** audiofiles;
+	int fcount;
+} ParsedLine;
+
+typedef struct LogData {
+	char* word;
+	int date;
+	int result;
+	double time; // review time taken in seconds
+} LogData;
+
+int get_day(void)
+{
+	time_t t = time(NULL);
+	struct tm date = *localtime(&t);
+	int today = (date.tm_year + 1900) * 10000 + (date.tm_mon + 1) * 100 + date.tm_mday;
+	return today;
+}
+
+ReviewData get_due(void)
+{
+	ReviewData reviews = {NULL, 0};
+	int today = get_day();
+
+	char path[128];
+	snprintf(path, sizeof(path), "%s%s%s", getenv("HOME"), CCQ_PATH, "deck");
+	FILE* deck = fopen(path, "r");
+	if (deck == NULL) {
+		perror("Deck access error");
+		exit(EXIT_FAILURE);
+	}
+
+	char line[MAX_LINE_LENGTH];
+	reviews.due = malloc(INITIAL_CAPACITY * sizeof(char *));
+	int capacity = INITIAL_CAPACITY;
+
+	while (fgets(line, sizeof(line), deck)) {
+		line[strcspn(line, "\n")] = 0;
+		char* last_pipe = strrchr(line, '|');
+		if (last_pipe != NULL) {
+			char* date_str = last_pipe + 1;
+			int date = atoi(date_str);
+			if (date <= today) {
+				if (reviews.wcount >= capacity) {
+					capacity += 100;
+					reviews.due = realloc(reviews.due, capacity * sizeof(char*));
+				}
+				reviews.due[reviews.wcount] = strdup(line);
+				++reviews.wcount;
+			}
+		}
+	}
+	fclose(deck);
+
+	return reviews;
+}
+
+ParsedLine parse_line(char* line) 
+{
+	ParsedLine parsed_line = {NULL, NULL, NULL, NULL, NULL, NULL, 0};
+
+	char* token = strtok(line, "|");
+	if (token != NULL) parsed_line.word = strdup(token);
+	printf("Debug: parsed_line.word assigned: '%s'\n", parsed_line.word);
+
+	token = strtok(NULL, "|");
+	if (token != NULL) parsed_line.reading = strdup(token);
+
+	token = strtok(NULL, "|");
+	if (token != NULL) parsed_line.definition = strdup(token);
+
+	token = strtok(NULL, "|");
+	if (token != NULL) parsed_line.sentence = strdup(token);
+
+	token = strtok(NULL, "|");
+	if (token != NULL) parsed_line.sreading = strdup(token);
+
+	token = strtok(NULL, "|");
+	if (token != NULL) {
+		int i = 0;
+		char** audiofiles = malloc(10 * sizeof(char*)); // Start with space for 10 paths
+		int capacity = 10;
+
+		char* audiopaths = strtok(token, ";");
+		if (audiopaths == NULL) parsed_line.audiofiles[0] = strdup(token);
+		while (audiopaths != NULL) {
+			if (i >= capacity) {
+				capacity += 10;
+				audiofiles = realloc(audiofiles, capacity * sizeof(char*));
+			}
+			char path[128];
+			snprintf(path, sizeof(path), "%s%s%s%s", getenv("HOME"), CCQ_PATH, "audio/", audiopaths);
+			audiofiles[i] = strdup(path);
+			++i;
+			audiopaths = strtok(NULL, ";");
+		}
+		parsed_line.audiofiles = audiofiles;
+		parsed_line.fcount = i;
+	}
+
+	return parsed_line;
+}
 
 int get_keypress(void) 
 {
-	// gets character without echoing it to the terminal
-	// and without requiring <Return>
-	// (thanks StackOverflow)
 	struct termios oldt, newt;
 	int ch;
 	tcgetattr(STDIN_FILENO, &oldt);
@@ -30,108 +142,101 @@ int get_keypress(void)
 	return ch;
 }
 
-int main(int argc, char *argv[])
+int review_line(ParsedLine parsed_line)
 {
-	// get today as int YYYYMMDD
-	time_t t = time(NULL);
-	struct tm date = *localtime(&t);
-	int today = (date.tm_year + 1900) * 10000 + (date.tm_mon + 1) * 100 + date.tm_mday;
-	printf("Reviews for today: %d\n", today);
+	printf("【%s】的意思是...\n", parsed_line.word);
+	int result;
+	while (1) {
+		result = get_keypress();
+		if (result == 't' || result == 'w') {
+			printf("拼音：\t\t%s\n", parsed_line.reading);
+			printf("意义：\t\t%s\n", parsed_line.definition);
+			if (parsed_line.sentence != NULL) printf("例句：\t\t %s\n", parsed_line.sentence);
+			if (parsed_line.sreading != NULL) printf("例句的拼音：\t%s\n", parsed_line.sreading);
+			// randomly play from audio database
+			if (parsed_line.fcount > 1) {
+				char command[256];
+				srand(time(NULL));
+				int rdraw = (rand() % parsed_line.fcount) - 1;
+				snprintf(command, sizeof(command), "mpv --really-quiet '%s' >/dev/null 2>&1 &", parsed_line.audiofiles[rdraw]);
+				system(command);
+			} else if (parsed_line.fcount == 1) {
+				char command[256];
+				snprintf(command, sizeof(command), "mpv --really-quiet '%s' >/dev/null 2>&1 &", parsed_line.audiofiles[0]);
+				system(command);
+			}
+			for (int i = 0; i < parsed_line.fcount; ++i) {
+				free(parsed_line.audiofiles[i]);
+			}
+			free(parsed_line.audiofiles);
+			break;
+		} else {
+			printf("无法识别的输入。\n");
+		}
+	}
+	free(parsed_line.word);
+	free(parsed_line.reading);
+	free(parsed_line.definition);
+	free(parsed_line.sentence);
+	free(parsed_line.sreading);
 
-	// load deck
+	return result;
+}
+
+void log_reviews(LogData* logs, int count)
+{
 	char path[128];
-	snprintf(path, sizeof(path), "%s%s", getenv("HOME"), RELATIVE_PATH);
-	FILE* deck = fopen(path, "a+");
-	if (deck == NULL) {
-		perror("Deck access error");
+
+	snprintf(path, sizeof(path), "%s%s%s", getenv("HOME"), CCQ_PATH, "log");
+	FILE* log = fopen(path, "a");
+	if (log == NULL) {
+		perror("Log access error");
 		exit(EXIT_FAILURE);
 	}
 
-	// get due words
-	char line[MAX_LINE_LENGTH];
-	char** matching_lines = malloc(INITIAL_CAPACITY * sizeof(char *));
-	int capacity = INITIAL_CAPACITY;
-	int count = 0;
-
-	while (fgets(line, sizeof(line), deck)) {
-		line[strcspn(line, "\n")] = 0; // remove \n from line
-		char* last_comma = strrchr(line, ',');
-		if (last_comma != NULL) {
-			// parse last csv field as review date
-			// copy line to memory if date <= today
-			char* date_str = last_comma + 1;
-			int date = atoi(date_str);
-			if (date <= today) {
-				if (count >= capacity) {
-					capacity += 100;
-					matching_lines = realloc(matching_lines, capacity * sizeof(char*));
-				}
-				matching_lines[count] = strdup(line);
-				count++;
-			}
-		}
+	for (int i = 0; i < count; ++i) {
+		printf("Debug: About to log: '%s'\n", logs[i].word);
+		fprintf(log, "%s|%d|%d|%0.2lf\n", logs[i].word, logs[i].date, logs[i].result, logs[i].time);
 	}
-	fclose(deck);
 
-	// review cards
-	if (count > 0) {	
-		CardReview reviews[count];
-		printf("Press [a]gain, [h]ard, [g]ood, [e]asy:\n");
-		int i = 0;
-		while (i < count) {
-			// parse first csv field as card id
-			char* first_comma = strchr(matching_lines[i], ',');
-			size_t id_length = first_comma - matching_lines[i];
-			char word_id[id_length + 1];
-			strncpy(word_id, matching_lines[i], id_length);
-			word_id[id_length] = '\0';
+	fclose(log);
+}
 
-			// parse second csv field as front of card
-			char* second_comma = strchr(first_comma + 1, ',');
-			size_t length_word = second_comma - first_comma - 1;
-			char word[length_word + 1];
-			strncpy(word, first_comma + 1, length_word);
-			word[length_word] = '\0';
+int main(int argc, char *argv[])
+{
+	setlocale(LC_ALL, "");
+	ReviewData reviews = get_due();
+	LogData reviews_log[reviews.wcount];
+	int i = 0;
 
-			// parse third csv field as reading
-			char* third_comma = strchr(second_comma + 1, ',');
-			size_t length_reading = third_comma - second_comma - 1;
-			char reading[length_reading + 1];
-			strncpy(reading, second_comma + 1, length_reading);
-			reading[length_reading] = '\0';
-
-			// parse fourth csv field as meaning
-			char* fourth_comma = strchr(third_comma + 1, ',');
-			size_t length_meaning = fourth_comma - third_comma - 1;
-			char meaning[length_meaning + 1];
-			strncpy(meaning, third_comma + 1, length_meaning);
-			meaning[length_meaning] = '\0';
-
-			// record user input for each card
-			printf("%s\n", word);
-			int review_input;
-			while (1) {
-				review_input = get_keypress();
-				if (review_input == 'a' || review_input == 'h' || review_input == 'g' || review_input == 'e') {
-					printf("Card ID %s is read %s and means %s\n\n", word_id, reading, meaning);
-					// later include extra_definitions; audio; sentence audio; image
-					// store id and review in array and move to next line
-					reviews[i].card_id = atoi(word_id);
-					reviews[i].review_input = review_input;
-					i++;
-					break;
-				} else {
-					// otherwise repeat
-					printf("Unrecognized input. Press [a], [h], [g] or [e]: ");
-				}
-			}
-		}
-		schedule_cards(reviews, count);
+	if (reviews.wcount == 0) {
+		printf("No reviews scheduled today\n");
+		return 0;
 	} else {
-		printf("No words to review.\n");
+		printf("今天有%d个单词要学习。准备好了吗？\n", reviews.wcount);
+		printf("按 [t]（通过）或 [w]（未通过）\n");
 	}
 
-	free(matching_lines);
+	while (i < reviews.wcount) {
+		LogData review_log = {NULL, 0, 0, 0.0};
+		struct timespec start, end;
+
+		ParsedLine parsed_line = parse_line(reviews.due[i]);
+		printf("Debug: Before assigning to review_log.word: '%s'\n", parsed_line.word);
+		review_log.word = parsed_line.word;
+		printf("Debug: After assigning to review_log.word: '%s'\n", review_log.word);
+		review_log.date = get_day();
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		review_log.result = review_line(parsed_line);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		review_log.time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+		reviews_log[i] = review_log;
+		free(reviews.due[i]);
+		++i;
+	}
+
+	log_reviews(reviews_log, reviews.wcount);
+	free(reviews.due);
 
 	return 0;
 }
