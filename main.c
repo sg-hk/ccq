d382f3951e86d6515b2c5e041c1724346ac56f18
sg-hk <rubenruben000@proton.me>
2025-04-17 01:07:12 +0800

    remove wchar.h

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <locale.h>
 #include <unistd.h>
 #include <termios.h>
-#include <wchar.h>
 #include <time.h>
 #include <stdlib.h>
 
@@ -10,6 +10,7 @@ static const char *FOX_DIR = "/.local/share/FOXP2/";
 static const int MAX_READ = 1024;
 static const int DUE_BUF = 128;
 static const int PATH_MAX = 256;
+static const int DATE_LEN = 10;
 
 char getrawch(void);
 
@@ -36,11 +37,11 @@ main(int argc, char *argv[])
         int pmonth = localnow->tm_mon + 1;
         int pday = localnow->tm_mday;
 
-	wchar_t *line = malloc(MAX_READ * sizeof(wchar_t));
+	uint32_t *line = malloc(MAX_READ * sizeof(uint32_t));
         if (!line) {perror("malloc line"); exit(1);}
 
-        wchar_t **fronts = malloc(DUE_BUF * sizeof(wchar_t*));
-        wchar_t **backs = malloc(DUE_BUF * sizeof(wchar_t*));
+        uint32_t **fronts = malloc(DUE_BUF * sizeof(uint32_t*));
+        uint32_t **backs = malloc(DUE_BUF * sizeof(uint32_t*));
         long *offsets = malloc(DUE_BUF * sizeof(long));
         if (!fronts || !backs || !offsets) {perror("malloc dues"); exit(1);}
 
@@ -48,43 +49,64 @@ main(int argc, char *argv[])
         long pos;
         size_t chars_read = 0;
 
-        /* we are using UTF-32 files to ensure rewriting at byte offsets works 
-         * fgetws won't work --> we need to manually parse the lines */
-        while (fread(&line[chars_read], sizeof(wchar_t), 1, slist) == 1) {
-                if (line[chars_read] != L'\n' && line[chars_read] != EOF) {
+        /* dumb and fun manual parsing */
+        while (fread(&line[chars_read], sizeof(uint32_t), 1, slist) == 1) {
+                if (line[chars_read] != (uint32_t)'\n' && line[chars_read] != EOF) {
                         ++chars_read;
                         continue;
                 }
 
-                line[chars_read + 1] = L'\0';
-                printf("More lines!\n");
-                /* go back chars_read + the new line */
-                pos = ftell(slist) - (chars_read + 1) * sizeof(wchar_t);
+                line[chars_read + 1] = (uint32_t)'\0';
+                chars_read = 0; // reset for other line counts
 
-                wchar_t *state1, *state2;
-
-                wchar_t *front = wcstok(line, L"|", &state1);
-                wchar_t *back = wcstok(NULL, L"|", &state1);
-                wchar_t *date = wcstok(NULL, L"|", &state1);
-
-                long date_offset = pos;
-                date_offset += wcslen(front) + 1 + wcslen(back) + 1;
-
-                int year = (int)wcstol(wcstok(date, L"/", &state2), NULL, 10);
-                int month = (int)wcstol(wcstok(NULL, L"/", &state2), NULL, 10);
-                int day = (int)wcstol(wcstok(NULL, L"/", &state2), NULL, 10);
-
-                if (year > pyear ||
-                                (year == pyear && month > pmonth) ||
-                                (year == pyear && month == pmonth && day > pday)) {
-                        chars_read = 0;
+                int idx = 0;
+                while(line[idx] != (uint32_t)'/')
+                        ++idx;
+                
+                int year = 0, month = 0, day = 0;
+                for (int i = 0; i < 4; ++i)
+                        year = year * 10 + line[idx-4+i] - '0';
+                if (year > pyear)
                         continue;
+                if (year == pyear) {
+                        for (int i = 0; i < 2; ++i) {
+                                month = month * 10 + line[idx+1+i] - '0';
+                                day = day * 10 + line[idx+4+i] - '0';
+                        }
+                        if (month > pmonth)
+                                continue;
+                        if (month == pmonth && day > pday)
+                                continue;
                 }
+                
+                /* go back to line start, +1 because we read \n too */
+                pos = ftell(slist) - (chars_read + 1)*sizeof(uint32_t);
+                /* go forward to date start */
+                pos += idx - 4;
+                offsets[due_n] = pos;
 
+                fronts[due_n] = malloc(MAX_READ*sizeof(uint32_t));
+                if (!fronts[due_n]) {perror("malloc fronts[due]"); exit(1);}
+                int idx2 = 0;
+                while(line[idx2] != (uint32_t)'|') {
+                        fronts[due_n][idx2] = line[idx2];
+                        ++idx2;
+                }
+                fronts[due_n][idx2] = (uint32_t)'\0';
+
+                /* now we have idx2 at | and idx at / 
+                 * and_we_want|THIS_FIELD|YYYY/ */
+                backs[due_n] = malloc(MAX_READ*sizeof(uint32_t));
+                if (!backs[due_n]) {perror("malloc backs[due]"); exit(1);}
+                for (int i = 0; i < (idx-(idx2+1)-6); ++i)
+                        backs[due_n][i] = line[idx2+1+i];
+                backs[due_n][idx-(idx2+1)-6+1] = (uint32_t)'\0';
+
+                ++due_n; // increment at end to turn from index to counter
                 if (due_n >= due_buf) {
                         due_buf *= 2;
-                        wchar_t **tfronts = realloc(fronts, due_buf * sizeof(wchar_t*));
-                        wchar_t **tbacks = realloc(backs, due_buf * sizeof(wchar_t*));
+                        uint32_t **tfronts = realloc(fronts, due_buf * sizeof(uint32_t*));
+                        uint32_t **tbacks = realloc(backs, due_buf * sizeof(uint32_t*));
                         long *toffs = realloc(offsets, due_buf * sizeof(long));
                         if (!tfronts || !tbacks || !toffs) 
                         {perror("realloc dues"); exit(1);}
@@ -92,33 +114,27 @@ main(int argc, char *argv[])
                         backs = tbacks;
                         offsets = toffs;
                 }
-
-
-                fronts[due_n] = wcsdup(front);
-                backs[due_n] = wcsdup(back);
-                offsets[due_n] = date_offset;
-                wprintf(L"Front: %ls, back: %ls, offset: %i\n",
-                                fronts[due_n], backs[due_n], offsets[due_n]);
-                wprintf(L"%i/%i/%i\n", year, month, day);
-                ++due_n;
-                chars_read = 0;
         }
         fclose(slist);
 
-        if (due_n == 0)
-        {printf("No cards due\n"); exit(0);}
+        if (due_n == 0) {
+                printf("No cards due\n"); 
+                exit(0);
+        }
 
         printf("There %s %i card%s due today.\n", 
-                        due_n > 1 ? "are" : "is", due_n, due_n > 1 ? "s" : "");
+               due_n > 1 ? "are" : "is", due_n, due_n > 1 ? "s" : "");
 
         printf("QUIZ: enter if pass, any to fail\n");
         int *incr = malloc(due_n * sizeof(int));
         char ch;
         for (int i = 0; i < due_n; ++i) {
-                wprintf(L"FRONT: %ls\n", fronts[i]);
+                for (int j = 0; fronts[i][j] != (uint32_t)'\0'; ++j)
+                        write(1, &fronts[i][j], sizeof(uint32_t));
                 free(fronts[i]);
                 getrawch();
-                wprintf(L"BACK: %ls\n", backs[i]);
+                for (int j = 0; backs[i][j] != (uint32_t)'\0'; ++j)
+                        write(1, &backs[i][j], sizeof(uint32_t));
                 free(backs[i]);
                 ch = getrawch();
                 incr[i] = (ch == '\n') ? 1 : 2;
@@ -134,15 +150,32 @@ main(int argc, char *argv[])
         for (int i = 0; i < due_n; ++i) {
                 time_t ntime = now + incr[i] * 86400;
                 struct tm *nlocal = localtime(&ntime);
-                wchar_t ndate[11]; // YYYY-MM-DD\0 = 11
-                swprintf(ndate, 11, L"%04d/%02d/%02d", 
-                                nlocal->tm_year+1900, nlocal->tm_mon+1, nlocal->tm_mday);
+                int nyear = nlocal->tm_year+1900;
+                int nmonth = nlocal->tm_mon+1;
+                int nday = nlocal->tm_mday;
 
-                if(fseek(slist, offsets[i] * sizeof(wchar_t), SEEK_SET))
-                {perror("fseek"); exit(1);}
+                uint32_t ndate[DATE_LEN + 1]; // YYYY/MM/DD\0 = 11
+                ndate[0] = nyear / 1000;
+                ndate[1] = (nyear - ndate[0]*1000) / 100;
+                ndate[2] = (nyear - ndate[0]*1000 - ndate[1]*100) / 10;
+                ndate[3] = nyear - ndate[0]*1000 - ndate[1]*100 - ndate[2]*10;
+                ndate[4] = (uint32_t)'/';
+                ndate[5] = nmonth / 10 ? nmonth / 10 : 0;
+                ndate[6] = nmonth - ndate[5];
+                ndate[7] = (uint32_t)'/';
+                ndate[8] = nday / 10 ? nday / 10 : 0;
+                ndate[9] = nday - ndate[8];
+                for (int i = 0; i < 10; ++i) {
+                        if (i != 4 && i != 7) 
+                                ndate[i] += '0';
+                }
+                ndate[10] = (uint32_t)'\0';
 
-                if(fwrite(ndate, sizeof(wchar_t), 10, slist) != 10)
-                {perror("fwrite ndate"); exit(1);}
+                if(fseek(slist, offsets[i]*sizeof(uint32_t), SEEK_SET))
+                        {perror("fseek"); exit(1);}
+
+                if(fwrite(ndate, sizeof(uint32_t), DATE_LEN, slist) != DATE_LEN)
+                        {perror("fwrite ndate"); exit(1);}
         }
 
         free(line);
@@ -157,20 +190,20 @@ char getrawch(void)
         char ch;
 
         if (tcgetattr(STDIN_FILENO, &old) == -1)
-        {perror("tcgetattr"); exit(1);}
+                {perror("tcgetattr"); exit(1);}
 
         new = old;
         new.c_lflag &= ~(ICANON | ECHO); // direct read + no echo
 
         if (tcsetattr(STDIN_FILENO, TCSANOW, &new) == -1)
-        {perror("tcsetattr"); exit(1);}
+                {perror("tcsetattr"); exit(1);}
 
         if (read(STDIN_FILENO, &ch, 1) != 1)
-        {perror("read"); exit(1);}
+                {perror("read"); exit(1);}
 
         /* restore old termios */
         if (tcsetattr(STDIN_FILENO, TCSANOW, &old) == -1)
-        {perror("tcsetattr"); exit(1);}
+                {perror("tcsetattr"); exit(1);}
 
         return ch;
 }
