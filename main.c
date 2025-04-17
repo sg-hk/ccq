4f975cdabef507a6f0307476f9889d15d65e3829
sg-hk <rubenruben000@proton.me>
2025-04-17 18:55:26 +0800

    fix date overwrite: char/byte counts, char_read=0, ndate[9]

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <locale.h>
 #include <unistd.h>
 #include <termios.h>
 #include <time.h>
 #include <stdlib.h>
 
-static const char *FOX_DIR = "/.local/share/FOXP2/";
+static const char *A122 = "/.local/share/A122/";
 static const int MAX_READ = 1024;
 static const int DUE_BUF = 128;
 static const int PATH_MAX = 256;
@@ -18,7 +17,6 @@ int to_utf8(uint32_t ch, char *out);
 int
 main(int argc, char *argv[])
 {
-        setlocale(LC_ALL, "");
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <study_list>\n", argv[0]);
 		exit(1);
@@ -27,10 +25,17 @@ main(int argc, char *argv[])
 	char path[PATH_MAX];
         char *home = getenv("HOME");
         if (!home) {perror("getenv('HOME')"); exit(1);}
-	snprintf(path, PATH_MAX, "%s%s%s", home, FOX_DIR, argv[1]);
+	snprintf(path, PATH_MAX, "%s%s%s", home, A122, argv[1]);
 	FILE *slist = fopen(path, "r");
-	if (!slist) 
-                {perror("fopen"); exit(1);}
+	if (!slist) {perror("fopen"); exit(1);}
+
+        uint32_t bom = 0;
+        fread(&bom, sizeof(uint32_t), 1, slist);
+        if (bom != 0x0000FEFF) {
+                fprintf(stderr, "%s is not UTF-32\n", argv[1]);
+                fclose(slist);
+                exit(1);
+        }
 
         time_t now = time(NULL);
         struct tm *localnow = localtime(&now);
@@ -48,37 +53,35 @@ main(int argc, char *argv[])
 
 	int due_n = 0, due_buf = DUE_BUF;
         long pos;
-        size_t chars_read = 0;
+        size_t ch_read = 0;
 
         /* dumb and fun manual parsing */
-        while (fread(&line[chars_read], sizeof(uint32_t), 1, slist) == 1) {
-                if (line[chars_read] != (uint32_t)'\n' && line[chars_read] != EOF) {
-                        ++chars_read;
+        while (fread(&line[ch_read], sizeof(uint32_t), 1, slist) == 1) {
+                if (ch_read >= MAX_READ -1) {
+                        fprintf(stderr, "line too long!\n");
+                        exit(1);
+                }
+
+                if (line[ch_read] != (uint32_t)'\n') {
+                        ++ch_read;
                         continue;
                 }
 
-                if (chars_read > MAX_READ) {
-                        fprintf(stderr, "line too long, skipping\n");
-                        chars_read = 0;
-                        continue;
-                }
+                line[ch_read + 1] = (uint32_t)'\0';
 
-                line[chars_read + 1] = (uint32_t)'\0';
-                chars_read = 0; // reset for other line counts
-
-                int idx = 0;
-                while(line[idx] != (uint32_t)'/')
-                        ++idx;
+                int slash = 0;
+                while(line[slash] != (uint32_t)'/')
+                        ++slash;
                 
                 int year = 0, month = 0, day = 0;
                 for (int i = 0; i < 4; ++i)
-                        year = year * 10 + line[idx-4+i] - '0';
+                        year = year * 10 + line[slash-4+i] - '0';
                 if (year > pyear)
                         continue;
                 if (year == pyear) {
                         for (int i = 0; i < 2; ++i) {
-                                month = month * 10 + line[idx+1+i] - '0';
-                                day = day * 10 + line[idx+4+i] - '0';
+                                month = month * 10 + line[slash+1+i] - '0';
+                                day = day * 10 + line[slash+4+i] - '0';
                         }
                         if (month > pmonth)
                                 continue;
@@ -87,52 +90,59 @@ main(int argc, char *argv[])
                 }
                 
                 /* go back to line start, +1 because we read \n too */
-                pos = ftell(slist) - (chars_read + 1)*sizeof(uint32_t);
+                pos = ftell(slist) - (ch_read + 1)*sizeof(uint32_t);
+                ch_read = 0; // reset for other line counts
                 /* go forward to date start */
-                pos += idx - 4;
+                pos += (slash - 4)*sizeof(uint32_t);
                 offsets[due_n] = pos;
 
                 fronts[due_n] = malloc(MAX_READ*sizeof(uint32_t));
                 if (!fronts[due_n]) {perror("malloc fronts[due]"); exit(1);}
-                int idx2 = 0;
-                while(line[idx2] != (uint32_t)'|') {
-                        fronts[due_n][idx2] = line[idx2];
-                        ++idx2;
+                int pipe = 0;
+                while(line[pipe] != (uint32_t)'|') {
+                        fronts[due_n][pipe] = line[pipe];
+                        ++pipe;
                 }
-                fronts[due_n][idx2] = (uint32_t)'\0';
+                fronts[due_n][pipe] = (uint32_t)'\0';
 
-                /* idx points to '/', idx2 points to '|'
-                 * back field is from idx2+1 to idx-6 (for |YYYY/) */
+                /* slash points to '/', pipe points to '|'
+                 * back field is from pipe+1 to slash-6 for |YYYY/ */
                 backs[due_n] = malloc(MAX_READ*sizeof(uint32_t));
                 if (!backs[due_n]) {perror("malloc backs[due]"); exit(1);}
-                for (int i = 0; i < (idx-(idx2+1)-6); ++i)
-                        backs[due_n][i] = line[idx2+1+i];
-                backs[due_n][idx-(idx2+1)-6+1] = (uint32_t)'\0';
+                for (int i = 0; i < (slash-(pipe+1)-5); ++i)
+                        backs[due_n][i] = line[pipe+1+i];
+                backs[due_n][slash-(pipe+1)-5+1] = (uint32_t)'\0';
 
                 ++due_n; // increment at end to turn from index to counter
                 if (due_n >= due_buf) {
                         due_buf *= 2;
-                        uint32_t **tfronts = realloc(fronts, due_buf * sizeof(uint32_t*));
-                        uint32_t **tbacks = realloc(backs, due_buf * sizeof(uint32_t*));
+                        uint32_t **tfr = realloc(fronts, due_buf * sizeof(uint32_t*));
+                        uint32_t **tbk = realloc(backs, due_buf * sizeof(uint32_t*));
                         long *toffs = realloc(offsets, due_buf * sizeof(long));
-                        if (!tfronts || !tbacks || !toffs) 
-                        {perror("realloc dues"); exit(1);}
-                        fronts = tfronts;
-                        backs = tbacks;
+                        if (!tfr || !tbk || !toffs) {
+                                perror("realloc dues"); 
+                                exit(1);
+                        }
+                        fronts = tfr;
+                        backs = tbk;
                         offsets = toffs;
                 }
         }
         fclose(slist);
+        free(line);
 
         if (due_n == 0) {
                 printf("No cards due\n"); 
+                free(fronts);
+                free(backs);
+                free(offsets);
                 exit(0);
         }
 
         printf("There %s %i card%s due today.\n", 
                due_n > 1 ? "are" : "is", due_n, due_n > 1 ? "s" : "");
 
-        printf("QUIZ: enter if pass, any to fail\n");
+        printf("QUIZ: enter if pass, any to fail\n\n");
         int *incr = malloc(due_n * sizeof(int));
         int len = 0;
         char ch, utf8[4];
@@ -142,14 +152,17 @@ main(int argc, char *argv[])
                         write(1, utf8, len);
                 }
                 free(fronts[i]);
+                write(1, "\n", 1);
                 getrawch();
                 for (int j = 0; backs[i][j] != (uint32_t)'\0'; ++j) {
                         len = to_utf8(backs[i][j], utf8);
                         write(1, utf8, len);
                 }
                 free(backs[i]);
+                write(1, "\n", 1);
                 ch = getrawch();
                 incr[i] = (ch == '\n') ? 1 : 2;
+                printf("%s\n\n", (ch == '\n') ? "PASS" : "FAIL");
         }
 
         free(fronts);
@@ -157,8 +170,7 @@ main(int argc, char *argv[])
 
         /* reopen slist, update the new dates */
         slist = fopen(path, "r+");
-        if (!slist) 
-        {perror("2nd fopen"); exit(1);}
+        if (!slist) {perror("2nd fopen"); exit(1);}
         for (int i = 0; i < due_n; ++i) {
                 time_t ntime = now + incr[i] * 86400;
                 struct tm *nlocal = localtime(&ntime);
@@ -176,28 +188,33 @@ main(int argc, char *argv[])
                 ndate[6] = nmonth - ndate[5];
                 ndate[7] = (uint32_t)'/';
                 ndate[8] = nday / 10 ? nday / 10 : 0;
-                ndate[9] = nday - ndate[8];
+                ndate[9] = nday - ndate[8] * 10;
                 for (int i = 0; i < 10; ++i) {
                         if (i != 4 && i != 7) 
                                 ndate[i] += '0';
                 }
                 ndate[10] = (uint32_t)'\0';
 
-                if(fseek(slist, offsets[i]*sizeof(uint32_t), SEEK_SET))
-                        {perror("fseek"); exit(1);}
+                /* note that the offsets are already in bytes
+                 * no need to multiply by sizeof() */
+                if(fseek(slist, offsets[i], SEEK_SET)) {
+                        perror("fseek"); 
+                        exit(1);
+                }
 
-                if(fwrite(ndate, sizeof(uint32_t), DATE_LEN, slist) != DATE_LEN)
-                        {perror("fwrite ndate"); exit(1);}
+                if(fwrite(ndate, sizeof(uint32_t), DATE_LEN, slist) 
+                   != DATE_LEN) {
+                        perror("fwrite ndate"); 
+                        exit(1);
+                }
         }
 
-        free(line);
-        free(offsets);
         free(incr);
+        free(offsets);
         exit(0);
 }
 
-char 
-getrawch(void)
+char getrawch(void)
 {
         struct termios old, new;
         char ch;
@@ -221,8 +238,7 @@ getrawch(void)
         return ch;
 }
 
-int 
-to_utf8(uint32_t ch, char *out)
+int to_utf8(uint32_t ch, char *out)
 {
     if (ch <= 0x7F) {
         out[0] = (char)ch;
